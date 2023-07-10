da5b2235e397a71aedb02db8222c7c562a04d114
NatalyaMaterinskaya <natulka-91@ukr.net>
2023-07-10 17:23:24 +0300

    Finished task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,44 +5,42 @@ const stepEl = formEl.elements.step;
 const amountOfPromiseEl = formEl.elements.amount;
 const btnEl = formEl.lastElementChild;
 
-
 btnEl.addEventListener('click', onStart);
 
 function onStart(event) {
   event.preventDefault();
   const objForPromise = {
-    position: amountOfPromiseEl.value,
-    delay: delayEl.value,
+    position: null,
+    delay: Number(delayEl.value),
   };
-  const step = stepEl.value;
-  console.log(step);
-  addPromise(objForPromise, step);
+  const amountOfPromise = Number(amountOfPromiseEl.value);
+  const step = Number(stepEl.value);
+  addPromise(objForPromise, amountOfPromise, step);
 }
 
-function addPromise({ position, delay }, step) {
-  let numDelay = Number(delay);
-  for (let i = 0; i < position; i += 1) {
-    setTimeout(() => {
-      console.log(numDelay);
-      createPromise(position, delay)
-        .then((position, delay) => {
-          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        })
-        .catch((position, delay) => {
-          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-        });
-    }, numDelay);
-    //numDelay += step;
+function addPromise({ position, delay }, amountOfPromise, step) {
+  for (let i = 1; i <= amountOfPromise; i += 1) {
+    position=i;
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
   }
 }
 
 function createPromise(position, delay) {
   return new Promise((res, rej) => {
-    const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      res(position, delay);
-    } else {
-      rej(position, delay);
-    }
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        res({position, delay });
+      } else {
+        rej({ position, delay });
+      }
+    },delay)
   });
 }
